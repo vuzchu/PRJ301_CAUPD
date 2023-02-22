3d1c0994a2ff0036661a4f63e26eecefecd990b4
vux <vucqhe150798@fpt.edu.vn>
2023-02-23 01:13:19 +0700

    Create Product.java

---
+++ PRJ301CAUPD/src/java/model/Product.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author vu
+ */
+public class Product {
+
+    private int product_id;
+    private String product_name;
+    private Date model_year;
+    private int list_price;
+    private String brand_name;
+    private String category_name;
+
+    public Product() {
+    }
+
+    public Product(int product_id, String product_name, Date model_year, int list_price, String brand_name, String category_name) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.model_year = model_year;
+        this.list_price = list_price;
+        this.brand_name = brand_name;
+        this.category_name = category_name;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Date getModel_year() {
+        return model_year;
+    }
+
+    public void setModel_year(Date model_year) {
+        this.model_year = model_year;
+    }
+
+    public int getList_price() {
+        return list_price;
+    }
+
+    public void setList_price(int list_price) {
+        this.list_price = list_price;
+    }
+
+    public String getBrand_name() {
+        return brand_name;
+    }
+
+    public void setBrand_name(String brand_name) {
+        this.brand_name = brand_name;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "product_id=" + product_id + ", product_name=" + product_name + ", model_year=" + model_year + ", list_price=" + list_price + ", brand_name=" + brand_name + ", category_name=" + category_name + '}';
+    }
+    
+
+}
